595f9323b3fe2473bbb0629fa968953f25ea25d1
Simon Chapman <eatyourpeasapps@gmail.com>
2015-08-28 21:50:44 +0100

    initial commit

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1447763372219642',
+    secret: 'dafc026028bbe2853a600d7ffb078fe1'
+});
